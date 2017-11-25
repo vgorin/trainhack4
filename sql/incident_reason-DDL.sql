07a6b5b0a13e3fd95706fb131474dd2240204010
vgorin <vgrn.at@gmail.com>
2017-11-25 19:37:59 +0200

    fix data type capacities

--- sql/incident_reason-DDL.sql
+++ sql/incident_reason-DDL.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS incident_reason;
 CREATE TABLE incident_reason
 (
-    `Incident Category` INT(11),
+    `Incident Category` VARCHAR(4),
     `Incident Reason` CHAR(2),
     `Incident Reason Name` VARCHAR(15),
     `Incident Category Description` VARCHAR(63),
     `Incident Reason Description` VARCHAR(128),
-    `Incident JPIP Category` VARCHAR(31),
+    `Incident JPIP Category` VARCHAR(63),
     `Incident Category Super Group Code` VARCHAR(4)
 );
--- sql/stanox_codes-DDL.sql
+++ sql/stanox_codes-DDL.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS stanox_codes;
 CREATE TABLE stanox_codes
 (
     `STANOX NO.` INT(11),
-    `FULL NAME` VARCHAR(31),
+    `FULL NAME` VARCHAR(63),
     `CRS CODE` CHAR(3),
     `NR ROUTE` VARCHAR(15)
 );
