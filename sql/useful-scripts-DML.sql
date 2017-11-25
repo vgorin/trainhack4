66c8797ce09be6464f78398bcf3a39376e0f5182
vgorin <vgrn.at@gmail.com>
2017-11-26 01:22:36 +0200

    scripts update

--- sql/useful-scripts-DML.sql
+++ sql/useful-scripts-DML.sql
@@ -3,3 +3,8 @@ SELECT DISTINCT d.`Start Stanox`, s1.`NR ROUTE`, d.`End Stanox`, s2.`NR ROUTE` F
 LEFT JOIN stanox_codes s1 on d.`Start Stanox`=s1.`STANOX NO.`
 LEFT JOIN stanox_codes s2 on d.`End Stanox` = s2.`STANOX NO.`
 WHERE s1.`NR ROUTE` <> s2.`NR ROUTE`;
+
+-- top 50 incidents:
+SELECT `Incident Number`, `Incident Start Datetime`, `Incident End Datetime`, count(`Incident Number`)
+FROM delays GROUP BY `Incident Number`, `Incident Start Datetime`, `Incident End Datetime`
+ORDER BY count(`Incident Number`) DESC LIMIT 50;
