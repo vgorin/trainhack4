de9b1280e6d30be63319de81cf29ec102588e3fb
vgorin <vgrn.at@gmail.com>
2017-11-25 19:21:30 +0200

    add Stanox Codes table data

---
+++ sql/stanox_codes-DDL.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS stanox_codes;
+CREATE TABLE stanox_codes
+(
+    `STANOX NO.` INT(11),
+    `FULL NAME` VARCHAR(31),
+    `CRS CODE` CHAR(3),
+    `NR ROUTE` VARCHAR(15)
+);
---
+++ sql/stanox_codes-import-DML.sql
@@ -0,0 +1,4 @@
+load data local infile 'stanox_codes.csv' into table stanox_codes
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\r';
