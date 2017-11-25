c67ec4105e136b0e3a0e1b188b76c4a440475b50
vgorin <vgrn.at@gmail.com>
2017-11-26 01:43:01 +0200

    scripts update

--- sql/useful-scripts-DML.sql
+++ sql/useful-scripts-DML.sql
@@ -8,3 +8,51 @@ WHERE s1.`NR ROUTE` <> s2.`NR ROUTE`;
 SELECT `Incident Number`, `Incident Start Datetime`, `Incident End Datetime`, count(`Incident Number`)
 FROM delays GROUP BY `Incident Number`, `Incident Start Datetime`, `Incident End Datetime`
 ORDER BY count(`Incident Number`) DESC LIMIT 50;
+
+-- generic script describing the column we're probably interested in
+SELECT
+  Date,
+  `Incident Number`,
+  `TSC - Affected`,
+  `English Day Type`,
+  `Incident Create Date`,
+  `Incident Start Datetime`,
+  `Incident End Datetime`,
+  `Section Code`,
+  d.`Incident Reason`,
+  `Incident Category`,
+  `Incident Category Super Group Code`,
+  `Incident Category Description`,
+  `Performance Event Code`,
+  `Start Stanox`,
+  `End Stanox`,
+  `Event Datetime`,
+  `PfPI Minutes`
+FROM delays d LEFT JOIN incident_reason i ON d.`Incident Reason` = i.`Incident Reason`
+WHERE `Incident Number` IN (
+  SELECT
+    `Incident Number`
+  FROM delays d LEFT JOIN incident_reason i ON d.`Incident Reason` = i.`Incident Reason`
+  WHERE
+    `Performance Event Code` NOT IN('C', 'O', 'P', 'S')
+    AND i.`Incident Category Super Group Code` IN ('NTAG', 'TAG', 'WSG', 'AG')
+    AND (`Section Code` LIKE '19215%' OR `Section Code` LIKE '%19215')
+    AND `Incident Start Datetime` = `Event Datetime`
+) AND `Incident Start Datetime` < `Event Datetime`;
+
+-- incidents caused by 12 incidents originated in section 19215
+SELECT
+  DISTINCT `Start Stanox`, count(`Start Stanox`), avg(`PfPI Minutes`)
+FROM delays d LEFT JOIN incident_reason i ON d.`Incident Reason` = i.`Incident Reason`
+WHERE `Incident Number` IN (
+  SELECT
+    `Incident Number`
+  FROM delays d LEFT JOIN incident_reason i ON d.`Incident Reason` = i.`Incident Reason`
+  WHERE
+    `Performance Event Code` NOT IN('C', 'O', 'P', 'S')
+    AND i.`Incident Category Super Group Code` IN ('NTAG', 'TAG', 'WSG', 'AG')
+    AND (`Section Code` LIKE '19215%' OR `Section Code` LIKE '%19215')
+    AND `Incident Start Datetime` = `Event Datetime`
+) AND `Incident Start Datetime` < `Event Datetime`
+GROUP BY `Start Stanox`;
+
