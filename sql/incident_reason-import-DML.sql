f1ca3de0c8b7fb3b565af5c48a074f9b0c8e105a
vgorin <vgrn.at@gmail.com>
2017-11-25 19:21:06 +0200

    fix the SQL script typo

--- sql/incident_reason-import-DML.sql
+++ sql/incident_reason-import-DML.sql
@@ -1,4 +1,4 @@
-load data local infile 'incident_reason.csv' into table delays
+load data local infile 'incident_reason.csv' into table incident_reason
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\r';
