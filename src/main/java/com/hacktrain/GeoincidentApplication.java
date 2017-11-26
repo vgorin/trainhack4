41b7b69d47f8863b9a9d9ceedf09fab375b5d4ba
vgorin <vgrn.at@gmail.com>
2017-11-26 02:31:32 +0200

    start work on the API: spring boot app

---
+++ src/main/java/com/hacktrain/GeoincidentApplication.java
@@ -0,0 +1,12 @@
+package com.hacktrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoincidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoincidentApplication.class, args);
+	}
+}
