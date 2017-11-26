d51bc0eb38662e61afba80381af1f182dea7fcd5
vgorin <vgrn.at@gmail.com>
2017-11-26 04:19:09 +0200

    fix resource packaging

--- src/main/java/com/trainhack/geoinc/GeoIncidentApplication.java
+++ src/main/java/com/trainhack/geoinc/GeoIncidentApplication.java
@@ -2,8 +2,10 @@ package com.trainhack.geoinc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.trainhack.geoinc")
 public class GeoIncidentApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/trainhack/geoinc/GeoIncidentApplicationTests.java
+++ src/test/java/com/trainhack/geoinc/GeoIncidentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hacktrain;
+package com.trainhack.geoinc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GeoincidentApplicationTests {
+public class GeoIncidentApplicationTests {
 
 	@Test
 	public void contextLoads() {
